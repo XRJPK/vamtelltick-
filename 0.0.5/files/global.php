4a7e9e369ace87b9d41446bbaa4d0f878341dca5
XRJPK <xrjpk@gmx.de>
2015-01-18 15:18:44 +0100

    Bug fix build

---
+++ 0.0.5/files/global.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Sebastian Heldt
+ * @copyright 2014 Sebastian Heldt
+ * @license	
+ * @package	de.getpoint.getpoint
+ */
+// include config
+require_once(__DIR__.'/config.inc.php');
+
+// include wcf
+require_once(RELATIVE_WCF_DIR.'global.php');
---
+++ 0.0.5/files/lib/page/ManagedSkillPage.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace cetos\page;
+use wcf\page\AbstractPage;
+
+
+class ManagedSkillPage extends AbstractPage { 
+
+public $activeMenuItem = 'cetos.header.menu.consulting';
+
+
+}
